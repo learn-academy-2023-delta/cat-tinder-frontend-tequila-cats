1869245278686360b175b3418c597cfcb9d0ebc5
AJCodes1 <etsncamacho@gmail.com>
2023-06-28 11:14:52 -0700

    fixed indentation and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,11 @@
 import React from 'react'
-import {Nav, NavItem, NavLink} from 'reactstrap'
+
 const Footer = () => {
   return (
     <>
-    <Nav pills>
-    <NavItem>
-    <NavLink
-    active
-    href="#"
-    >
-    Link
-    </NavLink>
-    </NavItem>
-    <NavItem>
-    <NavLink href="#">
-    Link
-    </NavLink>
-    </NavItem>
-    <NavItem>
-    <NavLink href="#">
-    Another Link
-    </NavLink>
-    </NavItem>
-    <NavItem>
-    <NavLink
-    disabled
-    href="#"
-    >
-    Disabled Link
-    </NavLink>
-    </NavItem>
-    </Nav>
+      <Footer>
+        <p>&copy; 2023 Cat Tinder | Jesus and Miguel</p>
+      </Footer>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,25 @@ import {Nav, NavItem, NavLink} from 'reactstrap'
 const Header = () => {
   return (
     <>
-    <Nav
-    fill
-    pills
-    >
-    <NavItem>
-      <NavLink
-        active
-        href="#"
-        >
-        Link
-      </NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink href="#">
-       Link
-      </NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink href="#">
-        Link
-      </NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink
-        disabled
-        href="#"
-        >
-        Link
-      </NavLink>
-    </NavItem>
-  </Nav>
+      <Nav fill pills>
+        <NavItem>
+          <NavLink active href="#">
+            Cat
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="#">
+            Add New Cat
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="#">
+            Sponsor a cat 
+          </NavLink>
+        </NavItem>
+      </Nav>
     </>
   )
 }
